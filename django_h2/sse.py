f24fd1e609e85707c463c2e0d5b5b75bc1d51ee0
kozzztik <kozzztik@mail.ru>
2024-01-25 01:15:20 +0300

    add test utils

--- django_h2/sse.py
+++ django_h2/sse.py
@@ -5,7 +5,8 @@ from django_h2.protocol import H2StreamingResponse
 class SSEResponse(H2StreamingResponse):
     def __init__(self, request: H2Request, handler):
         self.handler = handler
-        self.request = request
+        self.protocol = request.h2_protocol
+        self.stream_id = request.h2_stream_id
         super().__init__(
             status=200, content_type='text/event-stream',
             headers={
@@ -15,7 +16,7 @@ class SSEResponse(H2StreamingResponse):
 
     def close(self):
         super().close()
-        self.request.h2_protocol.end_stream(self.request.h2_stream_id)
+        self.protocol.end_stream(self.stream_id)
 
     async def send_event(self, name: str, data: str, event_id: str = None):
         event = [
@@ -25,6 +26,4 @@ class SSEResponse(H2StreamingResponse):
         if event_id is not None:
             event.append('id: ' + str(event_id).replace('\n', r'\n'))
         event = '\n'.join(event).encode() + b'\n\n'
-        await self.request.h2_protocol.send_data(
-            event,
-            self.request.h2_stream_id, end_stream=False)
+        await self.protocol.send_data(event, self.stream_id, end_stream=False)
